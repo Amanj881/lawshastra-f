2c0b00550c4aa1109df27fa85046089d29fa3ec9
bob <jainaman091996@gmail.com>
2020-12-08 12:57:59 +0530

    add transition

--- src/user/index.js
+++ src/user/index.js
@@ -6,41 +6,48 @@ import Footer from '../components/Footer/Footer.js'
 import styled from 'styled-components';
 import Motion from '../components/Motion/Motion.js'
 
-const TileWrapper = styled.div`
-  background: #fff;
-  margin: 2rem auto;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-  border-radius: 5px;
-  transform: ${props => `scale(${props.scale})`};
-`;
+const headerOptions = [
+{
+	label:'Home',
+	value:'home'
+},
+{
+	label:'Blogs',
+	value:'blog'
+},
+{
+	label:'Question Bank',
+	value:'question-bank'
+},
+]
 
 function Index(props) {
-	    const { scale } = props;
+	const { scale } = props;
 
 	return (
 		   
 		<div>
 		<Motion key="header" delay={300} from={"left"}>
-			<Header />
+			<Header headerOptions={headerOptions}/>
 			<Heros />
 			<hr className="h-2 sharow-lg"/>
 			</Motion>
 			<Motion key="blog" delay={2000} from={"right"}>
 			<Card label="Blog"/>
 			</Motion>
-			<Motion key="blog" delay={2500} from={"bottom"}>
+			<Motion key="cop" delay={2500} from={"bottom"}>
 			<hr className="h-2 sharow-lg"/>
 			<Card label="Call For Paper"/>
 			</Motion>
-			<Motion key="blog" delay={3000} from={"left"}>
+			<Motion key="webinar" delay={3000} from={"left"}>
 			<hr className="h-2 sharow-lg"/>
 			<Card label="Webinar"/>
 			</Motion>
-			<Motion key="blog" delay={3500} from={"top"}>
+			<Motion key="seminar" delay={3500} from={"top"}>
 			<hr className="h-2 sharow-lg"/>
 			<Card label="Seminars"/>
 			</Motion>
-			<Motion key="blog" delay={4000} from={"left"}>
+			<Motion key="footer" delay={4000} from={"left"}>
 			<hr className="h-2 sharow-lg"/>
 			<Footer />
 			</Motion>
